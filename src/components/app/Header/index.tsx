550c623935707b8d55eec1ff8427c05e4067c70b
Wadhah Mahrouk <wadhah.mahroug15@gmail.com>
2021-04-20 22:20:39 +0100

    added router logic to color change

--- src/components/app/Header/index.tsx
+++ src/components/app/Header/index.tsx
@@ -1,18 +1,26 @@
+import withRouter, { WithRouterProps } from 'next/dist/client/with-router'
 import React, { useEffect, useState } from 'react'
 import StatelessHeader from './StatelessHeader'
 
-interface Props {}
+// TODO program
+const pageWithScrollColorChangeChecker = (current: string): boolean =>
+  current === '/'
 
-export const Header: React.FC<Props> = () => {
-  const [textWhite, setTextWhite] = useState({ white: true })
+const Header: React.FC<WithRouterProps> = ({ router }) => {
+  const pageWithScrollColorChange = pageWithScrollColorChangeChecker(
+    router.pathname
+  )
+  const [textWhiteWithScroll, setTextWhite] = useState({ white: true })
   useEffect(() => {
+    if (!pageWithScrollColorChange) return () => null
+    const threshold = window.innerWidth > 600 ? window.innerHeight - 100 : 300
     let lastKnownScrollPosition = 0
     let ticking = false
 
     const doSomething = (scrollPos: number) => {
-      if (scrollPos > window.innerHeight && textWhite.white)
+      if (scrollPos > threshold && textWhiteWithScroll.white)
         setTextWhite({ white: false })
-      else if (scrollPos <= window.innerHeight && !textWhite.white) {
+      else if (scrollPos <= threshold && !textWhiteWithScroll.white) {
         setTextWhite({ white: true })
       }
     }
@@ -30,11 +38,13 @@ export const Header: React.FC<Props> = () => {
     }
     document.addEventListener('scroll', work)
     return () => document.removeEventListener('scroll', work)
-  }, [textWhite])
+  }, [textWhiteWithScroll])
   return (
     <StatelessHeader
-      text={textWhite}
+      text={pageWithScrollColorChange && textWhiteWithScroll}
       body={{ transparent: true, fixed: true }}
     />
   )
 }
+
+export default withRouter(Header)
