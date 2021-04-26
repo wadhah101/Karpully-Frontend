59a0dc4bc1a4d33f66af89287880e10bd071eccd
Wadhah Mahrouk <wadhah.mahroug15@gmail.com>
2021-04-26 15:43:34 +0100

    added analyser

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
@@ -18,4 +22,4 @@ module.exports = {
   future: {
     webpack5: true,
   },
-}
+})
